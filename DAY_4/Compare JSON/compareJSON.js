c7e6fc4a487f4ab232cb41d9f322ec5df4cb2f29
SnehaPriyaK <92200363+SnehaPriyaK@users.noreply.github.com>
2021-12-04 01:42:40 +0530

    Delete DAY_4 directory

--- DAY_4/Compare JSON/compareJSON.js
+++
@@ -1,4 +0,0 @@
-//Question: How to compare two JSON have the same properties without order?
-var obj1 = { name: "Person 1", age:5 };
-var obj2 = { age:5, name: "Person 1" };
-console.log(_.isEqual(obj1, obj2));
